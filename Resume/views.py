b62b4f3d1125b0061c0079f0e1e518d8738de271
Philani Mxathule <3911755@myuwc.ac.za>
2022-11-15 11:53:18 +0200

    working

--- Resume/views.py
+++ Resume/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "templates/index.html")
+    return render(request, "Resume/index.html")
 
 def philani(request):
     return HttpResponse("Hello Philani")
@@ -12,5 +12,10 @@ def philani(request):
 def lungas(request):
     return HttpResponse("Hello Lungas")
 
+# def greet(request, name):
+#     return HttpResponse(f"Hello, {name.capitalize()}!")
+
 def greet(request, name):
-    return HttpResponse(f"Resume/index.html, {name.capitalize()}!")
+    return render(request, "Resume/greet.html", {
+        "name": name.capitalize()
+    })
